e88f5ba994374098edf067ff5583996423418fb1
gggordon <ggg_gilroy@yahoo.com>
2017-11-08 14:00:39 -0500

    Started TreeNode class

---
+++ java/TreeNode.java
@@ -0,0 +1,15 @@
+public class TreeNode {
+	private int data;
+	private TreeNode left;
+	private TreeNode right;
+
+	public TreeNode(){
+		data = 0;
+		left = null;
+		right = null;
+	}
+
+	public TreeNode(){
+		
+	}
+}
